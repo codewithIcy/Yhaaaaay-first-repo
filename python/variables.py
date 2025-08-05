7414940479a0fc0515d901ebb708b75ca29b2b39
codewithIcy <ndedapaul57@gmail.com>
2025-08-05 22:26:09 +0300

    Variables pt1

---
+++ python/variables.py
@@ -0,0 +1,18 @@
+#Numerical Variables
+#Integers
+num = 7
+#float
+num1 = 7.5
+#complex
+num2 = 3 + 5j
+
+#string
+name = 'Icy'
+
+#Sequence
+#list
+groceries = ['Onions', 'Tomatoes', 'Eggs']
+print(groceries[2])
+#tuple
+cars = ('Mazda', 'Toyota', 'Ford')
+print(cars)
