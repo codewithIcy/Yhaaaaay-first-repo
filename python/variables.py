3a2ea0ed4f55a495961a7e34dee05c66347751b9
codewithIcy <ndedapaul57@gmail.com>
2025-08-06 21:29:45 +0300

    Variables Final

--- python/variables.py
+++ python/variables.py
@@ -16,3 +16,10 @@ print(groceries[2])
 #tuple
 cars = ('Mazda', 'Toyota', 'Ford')
 print(cars)
+print(cars[1])
+#set
+product_codes = {1,2,3,4}
+print(product_codes)
+#dictionary
+student_info = {"Michael": 15, "James": 10}
+print(student_info)
