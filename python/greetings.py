468e5f5222f05c2d6ab82ee3de9a2bc975fa8167
codewithIcy <ndedapaul57@gmail.com>
2025-08-06 23:47:03 +0300

    Greetins app

---
+++ python/greetings.py
@@ -0,0 +1,4 @@
+name = input("Hello, what is your name?:\n")
+favorite_color = input(f"Nice to meet you {name}, by the way if you don't mind me asking, what is your favourite color?:\n")
+
+print(f"Aaaaah, {favorite_color} is my favourite color too. Anyway I have to go {name}. It was nice chatting with you, bye!")
---
+++ python/operators.py
@@ -0,0 +1,17 @@
+#Arithmetic Operators
+#Addition
+print(5+3)
+#Subtraction
+print(2-1)
+#multiplication
+print(3*4)
+#exponential
+print(5**3)
+#float division
+print(6/3)
+#floor division
+print(15//7)
+#modulus
+print(17%3)
+
+#Comparison Operators
