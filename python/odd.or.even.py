4b4bbf8a07144c30bf18dc90fae46639bb14a4a2
codewithIcy <ndedapaul57@gmail.com>
2025-08-09 09:32:20 +0300

    Waaaaah sijui sasa😪

---
+++ python/odd.or.even.py
@@ -0,0 +1,18 @@
+#check if the number is even or odd.
+
+def checkifEvenorOdd(number):
+    if number % 2==0:
+        print(number, "is an even number")
+
+    else:
+        print(number, "is an odd number")
+
+while True:
+    count = 0
+    count =+ 1
+
+    print(int(count))
+    if count == 700:
+        print("Yaaaaaaay")
+    else:
+        continue
